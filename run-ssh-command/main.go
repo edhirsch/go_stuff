e4db6399d7a8e03e489a2c0638e65f6d891e395d
Eduard Hirsch -X (edhirsch - 3PILLAR GLOBAL INC at Cisco) <edhirsch@cisco.com>
2020-02-12 17:49:25 +0200

    changed vars to struct

--- run-ssh-command/main.go
+++ run-ssh-command/main.go
@@ -8,34 +8,37 @@ import (
 )
 
 //import "golang.org/x/crypto/ssh"
+type Server struct {
+	hostname string
+	username string
+	password []byte
+}
 
-var server string
-var user string
-var password []byte
+var s Server
 
 func main() {
 	inputConnectionDetails()
 	fmt.Println()
-	fmt.Printf("server:    %v\n", server)
-	fmt.Printf("user name: %v\n", user)
-	fmt.Printf("password:  %v\n", string(password))
+	fmt.Printf("server:    %v\n", s.hostname)
+	fmt.Printf("user name: %v\n", s.username)
+	fmt.Printf("password:  %v\n", string(s.password))
 
 }
 
 func inputConnectionDetails() {
 	fmt.Printf("Server name: ")
-	_, err := fmt.Scanln(&server)
+	_, err := fmt.Scanln(&s.hostname)
 	if err != nil {
 		os.Exit(1)
 	}
 
 	fmt.Printf("User name:   ")
-	_, err = fmt.Scanln(&user)
+	_, err = fmt.Scanln(&s.username)
 	if err != nil {
 		os.Exit(1)
 	}
 
 	fmt.Printf("Password:    ")
-	password, err = terminal.ReadPassword(0)
+	s.password, err = terminal.ReadPassword(0)
 	fmt.Println()
 }
