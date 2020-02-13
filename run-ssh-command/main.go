680fa380ffa84a1f6a0cb9dfe3acb5d2d1de5d54
Eduard Hirsch -X (edhirsch - 3PILLAR GLOBAL INC at Cisco) <edhirsch@cisco.com>
2020-02-13 17:53:43 +0200

    added ssh-simple-client

--- run-ssh-command/main.go
+++ run-ssh-command/main.go
@@ -4,24 +4,37 @@ import (
 	"fmt"
 	"os"
 
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-//import "golang.org/x/crypto/ssh"
-type Server struct {
+type server struct {
 	hostname string
 	username string
-	password []byte
+	password string
+	port     string
 }
 
-var s Server
+var s server
 
 func main() {
+
+	s.port = "22"
 	inputConnectionDetails()
 	fmt.Println()
 	fmt.Printf("server name: %v\n", s.hostname)
 	fmt.Printf("user name:   %v\n", s.username)
 	fmt.Printf("password:    %v\n", string(s.password))
+	fmt.Println()
+	fmt.Printf("Connecting to %v@%v:%v ..\n", s.username, s.hostname, s.port)
+
+	conn, err := createSSHConnection(s.hostname, s.username, s.password, s.port)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(conn)
+	fmt.Printf("%T\n", conn)
 
 }
 
@@ -39,6 +52,25 @@ func inputConnectionDetails() {
 	}
 
 	fmt.Printf("password:    ")
-	s.password, err = terminal.ReadPassword(0)
+	passwordB, err := terminal.ReadPassword(0)
+	s.password = string(passwordB)
 	fmt.Println()
+}
+
+func createSSHConnection(hostname string, username string, password string, port string) (interface{}, error) {
+	// Configure ssh client
+	sshConfig := &ssh.ClientConfig{
+		User: s.username,
+		Auth: []ssh.AuthMethod{ssh.Password(string(s.password))},
+	}
+
+	// Create ssh connection
+	connection, err := ssh.Dial("tcp", s.hostname+":"+s.port, sshConfig)
+	if err != nil {
+		fmt.Println("Connection failed ..")
+		return nil, fmt.Errorf("Failed to dial: %s", err)
+	}
+	fmt.Println("Connection succeeded !")
+
+	return connection, nil
 }
---
+++ ssh-simple-client/main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+)
+
+// exported Constants
+const (
+	CertPassword      = 1
+	CertPublicKeyFile = 2
+	DefaultTimeout    = 3 // second
+)
+
+// SSH function
+type SSH struct {
+	IP      string
+	User    string
+	Cert    string //password or key file path
+	Port    int
+	session *ssh.Session
+	client  *ssh.Client
+}
+
+func (sshClient *SSH) readPublicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
+
+// Connect function
+func (sshClient *SSH) Connect(mode int) {
+
+	var sshConfig *ssh.ClientConfig
+	var auth []ssh.AuthMethod
+	if mode == CertPassword {
+		auth = []ssh.AuthMethod{ssh.Password(sshClient.Cert)}
+	} else if mode == CertPublicKeyFile {
+		auth = []ssh.AuthMethod{sshClient.readPublicKeyFile(sshClient.Cert)}
+	} else {
+		log.Println("does not support mode: ", mode)
+		return
+	}
+
+	sshConfig = &ssh.ClientConfig{
+		User: sshClient.User,
+		Auth: auth,
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+		Timeout: time.Second * DefaultTimeout,
+	}
+
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sshClient.IP, sshClient.Port), sshConfig)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// session, err := client.NewSession()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	client.Close()
+	// 	return
+	// }
+
+	// sshClient.session = session
+	sshClient.client = client
+}
+
+// RunCmd function
+func (sshClient *SSH) RunCmd(cmd string) {
+	out, err := sshClient.session.CombinedOutput(cmd)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
+}
+
+// RefreshSession function
+func (sshClient *SSH) RefreshSession() {
+	session, err := sshClient.client.NewSession()
+	if err != nil {
+		fmt.Println(err)
+		sshClient.Close()
+		return
+	}
+
+	sshClient.session = session
+	sshClient.client = sshClient.client
+}
+
+// Shell function
+func (sshClient *SSH) Shell() {
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+	// Request pseudo terminal
+	if err := sshClient.session.RequestPty("xterm", 40, 80, modes); err != nil {
+		log.Fatal("request for pseudo terminal failed: ", err)
+	}
+	err := sshClient.session.Shell()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// Close function
+func (sshClient *SSH) Close() {
+	sshClient.session.Close()
+	sshClient.client.Close()
+}
+
+// InteractiveShell function
+func (sshClient SSH) InteractiveShell() {
+	cmd := ""
+	for cmd != "exit\n" {
+		fmt.Printf("[ %v@%v ]# ", sshClient.User, sshClient.IP)
+		reader := bufio.NewReader(os.Stdin)
+		cmd, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		sshClient.RunCmd(cmd)
+		sshClient.RefreshSession()
+	}
+}
+
+// main function
+func main() {
+	client := &SSH{
+		IP:   "csaf-ast-ui.cisco.com",
+		User: "root",
+		Port: 22,
+		Cert: "cisco123",
+	}
+	client.Connect(CertPassword)
+	client.RefreshSession()
+	client.InteractiveShell()
+	//client.RunCmd("ls /home")
+	client.Close()
+}
